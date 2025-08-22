b0cc316c9b8e0c43052e3c7b774caedf80a81456
Peron <peron_ap@icloud.com>
2025-08-22 12:56:52 +0100

    chore: organize project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
 import { Text } from 'ink';
+import { useEffect, useState } from 'react';
 
 export const App = () => {
   const [counter, setCounter] = useState(0);
